ddedb8dc0f733b5ade6563826a4f7165b144c1f3
Lindy Lindstrom <llindstrom@lco.global>
2021-06-14 13:30:44 -0700

    supply AWS_ACCESS_KEY_ID to S3 credentials

--- tom_demo_base/settings.py
+++ tom_demo_base/settings.py
@@ -211,7 +211,7 @@ CACHES = {
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', 'tomdemo.lco.global-rw')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_S3_BUCKET', 'tomdemo.lco.global')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-west-2')
