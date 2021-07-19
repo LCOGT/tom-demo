b1d38f07ef3ac17534ffcf1b7e1c1c77d07c4048
David Collom <dcollom@lco.global>
2021-07-19 14:56:43 -0700

    Added text filter

--- vue/src/components/AlertsTable.vue
+++ vue/src/components/AlertsTable.vue
@@ -1,33 +1,48 @@
 <template>
-    <b-table id="alerts-table" sticky-header hover head-variant="light" foot-clone :items="getAlertsFromAlertData()" :fields="alert_fields">
-        <template #cell(show_details)="data">
-            <b-link v-if="data.detailsShowing" @click="data.toggleDetails">
-                -
-            </b-link>
-            <b-link v-else @click="data.toggleDetails">
-                +
-            </b-link>
-        </template>
-        <template #cell(identifier)="data">
-            <b-link :href="getAlertUrl(data.value)">{{ data.value }}</b-link>
-            <!-- <a href="{{ getAlertUrl(data.value) }}">{{ data.value }}</a> -->
-        </template>
-        <!-- <template #cell(timestamp)="data">
-            {{ data.identifier }}
-        </template> -->
-        <template #cell(from)="data">
-            {{ data.item.parsed_message.from }}
-        </template>
-        <template #cell(subject)="data">
-            {{ data.item.parsed_message.subject }}
-        </template>
-        <template #foot()="data">
-            <span>{{ data.value }}</span>
-        </template>
-        <template #row-details="data">
-            <span>{{ data.item.parsed_message.body }}</span>
-        </template>
-    </b-table>
+    <div>
+        <b-row>
+            <b-form-group>
+                <b-form-input id="filter-input" v-model="filter" type="search" placeholder="Search Alerts" />
+            </b-form-group>
+        </b-row>
+        <b-table
+            id="alerts-table"
+            sticky-header
+            hover
+            head-variant="light"
+            foot-clone
+            :filter="filter"
+            :items="getAlertsFromAlertData()"
+            :fields="alert_fields"
+        >
+            <template #cell(show_details)="data">
+                <b-link v-if="data.detailsShowing" @click="data.toggleDetails">
+                    -
+                </b-link>
+                <b-link v-else @click="data.toggleDetails">
+                    +
+                </b-link>
+            </template>
+            <template #cell(identifier)="data">
+                <b-link :href="getAlertUrl(data.value)">{{ data.value }}</b-link>
+            </template>
+            <!-- <template #cell(timestamp)="data">
+                {{ data.identifier }}
+            </template> -->
+            <template #cell(from)="data">
+                {{ data.item.parsed_message.from }}
+            </template>
+            <template #cell(subject)="data">
+                {{ data.item.parsed_message.subject }}
+            </template>
+            <template #foot()="data">
+                <span>{{ data.value }}</span>
+            </template>
+            <template #row-details="data">
+                <span>{{ data.item.parsed_message.body }}</span>
+            </template>
+        </b-table>
+    </div>
 </template>
 
 <script>
@@ -45,6 +60,7 @@ export default {
                 { 'key': 'from' },
                 { 'key': 'subject' }
             ],
+            filter: null,
         }
     },
     props: {
