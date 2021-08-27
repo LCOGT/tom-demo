4b7ba75e3785bdb9f1ac3eca0086c7607490fe33
David Collom <dcollom@lco.global>
2021-08-26 21:25:21 -0700

    Removing CandidateForm

--- vue/src/components/index.js
+++ vue/src/components/index.js
@@ -1,7 +1,6 @@
 import AddCandidateModal from './AddCandidateModal.vue';
 import AlertsTable from './AlertsTable.vue';
-import CandidateForm from './CandidateForm.vue';
 import GravitationalWaveBanner from './GravitationalWaveBanner.vue';
 import SelectableTargetTable from './SelectableTargetTable.vue';
 
-export { AddCandidateModal, AlertsTable, CandidateForm, GravitationalWaveBanner, SelectableTargetTable };
+export { AddCandidateModal, AlertsTable, GravitationalWaveBanner, SelectableTargetTable };
