ee586fabddf4fafbc51648a43e6b213e59d0b63d
Lindy Lindstrom <llindstrom@lco.global>
2021-10-19 21:34:42 +0000

    organize import alphabetically (for dc)

--- vue/src/components/index.js
+++ vue/src/components/index.js
@@ -1,9 +1,10 @@
 import AddCandidateModal from './AddCandidateModal.vue';
 import AlertsTable from './AlertsTable.vue';
+import CandidateTargetTable from './CandidateTargetTable.vue';
 import CreateTargetModal from './CreateTargetModal.vue';
 import GravitationalWaveBanner from './GravitationalWaveBanner.vue';
 import SelectableTargetTable from './SelectableTargetTable.vue';
-import CandidateTargetTable from './CandidateTargetTable.vue';
+
 
 export {
     AddCandidateModal,
