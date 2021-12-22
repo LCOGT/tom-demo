cb4215065565e76fb133c805f747d49ce70ecd04
Lindy Lindstrom <llindstrom@lco.global>
2021-12-22 19:52:30 +0000

    cleaned-up, working version (sorting seems broken, hwvr)

--- vue/src/components/CandidateTargetTable.vue
+++ vue/src/components/CandidateTargetTable.vue
@@ -3,26 +3,33 @@
         <b-table
             striped
             :primary-key="id"
+            :sort-by="priority"
             :fields="candidateFields"
-            :items="filteredCandidates">
+            :items="candidates">
             <!-- see https://bootstrap-vue.org/docs/components/table#custom-data-rendering -->
 
-            <!-- A virtual column -->
-            <template #cell(index)="row">
-            {{ row.index }}
-            </template>
-
+            <!-- Set up the link to the TargetDetail page for this Candidates Target -->
             <template #cell(target-link)="row">
                 <b-link :href="getTargetDetailUrl(row.item.target)">{{ row.item.target.name }}</b-link>
             </template>
-            <template v-slot:cell(viable)="row">
-                <b-form-checkbox @change="$emit('toggle-viability', row, $event)"
-                v-model="row.item.viable"/>
+
+            <!-- Set up the Activate/Retire Button -->
+            <template #cell(active)="row">
+                <b-button size="sm" @click="$emit('toggle-viability', row, $event)">
+                    {{ row.item.viable ? 'Retire' : 'Activate'}}
+                </b-button>
             </template>
+
+            <!-- Set up Priority (NOT IMPLEMENTED AT THE MOMENT)
             <template #cell(priority)="row">
-                <b-form-spinbutton v-model="priority_value" value=1
+                <b-form-spinbutton
+                    inline
+                    size="sm"
+                    v-model="row.item.priority"
                 @change="$emit('change-priority', row, $event)" />
             </template>
+            -->
+
         </b-table>
     </b-container>
 </template>
@@ -32,42 +39,23 @@ export default {
     name: 'CandidateTargetTable',
     components: {},
     props: {
-      selectable: {
-          type: Boolean,
-          required: false,
-          default: true
-      },
       candidates: {
         type: Array,
         required: true
       },
-      showViable: {
-          type: Boolean,
-          required: false,
-          default: true
-      }
     },
     computed: {
-        filteredCandidates() {
-            return this.candidates.filter(
-                item => { return item.viable === this.showViable })
-        }
     },
     data() {
         return {
-            priority_value: 'priority',
             candidateFields: [
-                { 'key': 'priority', 'label': 'Priority', 'sortable': true },
+                //{ 'key': 'priority', 'label': 'Priority', 'sortable': true },
                 { 'key': 'target-link', 'label': 'Candidate', 'sortable': true },
-                'index',
-                { 'key': 'id', 'label': 'Superevent ID' },
-                { 'key': 'target.id', 'label': 'Target ID', 'sortable': true },
+                { 'key': 'superevent', 'label': 'Superevent', 'sortable': true },
                 { 'key': 'superevent.note', 'label': 'Note' },
-                { 'key': 'superevent.plan', 'label': 'Plan' },
-                { 'key': 'superevent.note_on_plan', 'label': 'Note on Plan' },
                 { 'key': 'target.ra', 'label': 'RA', formatter: (value, key, item) => value.toLocaleString() },
                 { 'key': 'target.dec', 'label': 'DEC', formatter: (value, key, item) => value.toLocaleString() },
-                { 'key': 'viable' },
+                { 'key': 'active' },
             ],
         }
     },
