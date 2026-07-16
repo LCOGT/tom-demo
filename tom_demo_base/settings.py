118eae46eea95447c355d57bded7e5e0e7265626
Joey Chatelain <jchate6@gmail.com>
2026-07-16 13:48:16 -0700

    update bootstrap setting

--- tom_demo_base/settings.py
+++ tom_demo_base/settings.py
@@ -114,8 +114,8 @@ TEMPLATES = [
     },
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 WSGI_APPLICATION = 'tom_demo_base.wsgi.application'
 
